f33817f7d7494ab91aa404612f9c31ac16a2c47b
eunyupp kim <fubugid12@naver.com>
2022-01-07 01:39:53 +0900

    소스코드작업 완료

---
+++ Calculator/RoundButton.swift
@@ -0,0 +1,23 @@
+//
+//  RoundButton.swift
+//  Calculator
+//
+//  Created by EunYupp Kim on 2022/01/06.
+//
+
+import UIKit
+
+@IBDesignable
+
+class RoundButton: UIButton {
+
+    @IBInspectable var isRound: Bool = false {
+        didSet {
+            if isRound {
+                self.layer.cornerRadius = self.frame.height / 2
+            }
+        }
+    }
+
+
+}
